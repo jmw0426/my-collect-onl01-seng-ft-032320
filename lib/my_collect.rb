f79d93687452f74bc7e01bb424b5d63b2348ffa5
Joshua Williams <jmw0426@hotmail.com>
2020-03-26 22:03:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,24 @@
+# def my_collect(array)
+# new_collection = []
+# i = 0
+# while i < array.length
+#   yield (array[i])
+#   i = i + 1
+#   i += 1
+#   my_collect(array) do |student| student.split(" ").first
+#   new_collection << array
+# end
+# new_collection
+# end
+# end
+
+
 def my_collect(array)
-new_collection = []
-i = 0
- while i < array.length
-  yield (array[i])
-  i = i + 1
-  i += 1
-  my_collect(array) do |student| student.split(" ").first
-  new_collection << array
- end
- new_collection
-end
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
