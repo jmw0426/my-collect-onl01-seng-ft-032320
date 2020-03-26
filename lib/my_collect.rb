02c306850fb38d9e274a74a0fe1eaa2ca9f62d52
Joshua Williams <jmw0426@hotmail.com>
2020-03-26 22:04:05 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,17 +1,3 @@
-# def my_collect(array)
-# new_collection = []
-# i = 0
-# while i < array.length
-#   yield (array[i])
-#   i = i + 1
-#   i += 1
-#   my_collect(array) do |student| student.split(" ").first
-#   new_collection << array
-# end
-# new_collection
-# end
-# end
-
 
 def my_collect(array)
   i = 0
